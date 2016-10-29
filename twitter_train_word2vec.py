b8e7f5003fb9ef7d47a908d9185186610585a62f
Flavio Martins <flaviomartins@acm.org>
2016-10-29 23:26:22 +0100

    re add python fallback for multi platform compatibility

--- twitter_train_word2vec.py
+++ twitter_train_word2vec.py
@@ -5,6 +5,8 @@ import os
 import sys
 import subprocess
 import fnmatch
+import tarfile
+import bz2
 import ujson as json
 import gensim
 from nltk.tokenize import TweetTokenizer
@@ -24,22 +26,31 @@ else:
     io_method = StringIO
 
 
+NATIVE_METHOD = 'native'
+COMPAT_METHOD = 'compat'
+
+
 class MultipleFileSentences(object):
     def __init__(self, basedir):
         self.basedir = basedir
         self.tokenizer = TweetTokenizer(preserve_case=False, reduce_len=True, strip_handles=True)
+        self.method = NATIVE_METHOD
         self.command = 'pbzip2'
         try:
             subprocess.Popen(['pbzip2', '--version'])
         except OSError:
-            subprocess.Popen(['bzip2', '--version'])
-            self.command = 'bzip2'
-        print 'will use ' + self.command
+            try:
+                subprocess.Popen(['bzip2', '--version'])
+                self.command = 'bzip2'
+            except OSError:
+                self.method = COMPAT_METHOD
+                self.command = 'python tarfile/bz2'
+        print 'method: ' + self.method + '\nusing: ' + self.command
 
     @staticmethod
-    def my_json_load(f):
+    def my_json_loads(content):
         try:
-            data = json.load(f)
+            data = json.loads(content)
         except ValueError:
             data = ''
         return data
@@ -49,19 +60,27 @@ class MultipleFileSentences(object):
             for filename in fnmatch.filter(filenames, '*.tar'):
                 fullfn = os.path.join(root, filename)
                 print fullfn
-                p1 = subprocess.Popen(['tar', 'xfO', fullfn, '--wildcards', '--no-anchored', '*.bz2'], stdout=subprocess.PIPE)
-                p2 = subprocess.Popen([self.command, '-d'], stdin=p1.stdout, stdout=subprocess.PIPE)
-                p1.stdout.close()
-                fh = io_method(p2.communicate()[0])
-                assert p2.returncode == 0
 
-                for line in fh:
-                    try:
-                        data = self.my_json_load(line)
+                if self.method == NATIVE_METHOD:
+                    p1 = subprocess.Popen(['tar', 'xfO', fullfn, '--wildcards', '--no-anchored', '*.bz2'], stdout=subprocess.PIPE)
+                    p2 = subprocess.Popen([self.command, '-d'], stdin=p1.stdout, stdout=subprocess.PIPE)
+                    p1.stdout.close()
+                    content = io_method(p2.communicate()[0])
+                    assert p2.returncode == 0
+                    for line in content:
+                        data = self.my_json_loads(line)
                         if 'text' in data:
                             yield self.tokenizer.tokenize(data['text'])
-                    except ValueError:
-                        pass
+                else:
+                    with tarfile.open(fullfn, 'r') as tar:
+                        for tarinfo in tar:
+                            if tarinfo.isfile() and os.path.splitext(tarinfo.name)[1] == ".bz2":
+                                f = tar.extractfile(tarinfo.name)
+                                content = StringIO(bz2.decompress(f.read()))
+                                for line in content:
+                                    data = self.my_json_loads(line)
+                                    if 'text' in data:
+                                        yield self.tokenizer.tokenize(data['text'])
 
 
 if __name__ == '__main__':
