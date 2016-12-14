b053d3e38b2a630cce031ee30751404ce857e0e4
Flavio Martins <flaviomartins@acm.org>
2016-12-14 16:18:41 +0000

    add more cli options

--- jsonl_kmeans.py
+++ jsonl_kmeans.py
@@ -28,13 +28,16 @@ def split_on_space(text):
     batch_size=("Batch size", "option", "c", int),
     job_size=("Job size in number of lines", "option", "j", int),
     max_docs=("Limit maximum number of documents", "option", "L", int),
-    max_features=("Maximum number of features (dimensions) to extract from text.", "option", "-max-features", int),
-    no_minibatch=("Use ordinary k-means algorithm (in batch mode).", "flag", "-no_minibatch", bool),
-    no_idf=("Disable Inverse Document Frequency feature weighting.", "flag", "-no_idf", bool),
-    verbose=("Print progress reports inside k-means algorithm.", "flag", "verbose", bool)
+    max_features=("Maximum number of features (dimensions) to extract from text.", "option", "D", int),
+    no_minibatch=("Use ordinary k-means algorithm (in batch mode).", "flag", "nm", bool),
+    binary_tf=("Make tf term in tf-idf binary.", "flag", "b", bool),
+    sublinear_tf=("Apply sublinear tf scaling, i.e. replace tf with 1 + log(tf).", "flag", "l", bool),
+    no_idf=("Disable Inverse Document Frequency feature weighting.", "flag", "ni", bool),
+    verbose=("Print progress reports inside k-means algorithm.", "flag", "v", bool)
 )
 def main(in_dir, out_loc, n_workers=cpu_count()-1, nr_clusters=10, batch_size=1000, nr_iter=100,
-         job_size=1, max_docs=None, max_features=10000, no_minibatch=False, no_idf=False, verbose=False):
+         job_size=1, max_docs=None, max_features=10000, no_minibatch=False,
+         binary_tf=False, sublinear_tf=False, no_idf=False, verbose=False):
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
     minibatch = not no_minibatch
     use_idf = not no_idf
@@ -51,9 +54,9 @@ def main(in_dir, out_loc, n_workers=cpu_count()-1, nr_clusters=10, batch_size=10
                                  preprocessor=None, tokenizer=split_on_space, analyzer='word',
                                  stop_words=None, token_pattern=None,
                                  max_df=0.5, min_df=5,
-                                 max_features=max_features, vocabulary=None, binary=True,  # binary=True -> tf=1 cap
+                                 max_features=max_features, vocabulary=None, binary=binary_tf,  # binary_tf -> tf=1 cap
                                  norm='l2', use_idf=use_idf, smooth_idf=True,
-                                 sublinear_tf=False)  # sublinear_tf=True -> tf = 1 + log(tf)
+                                 sublinear_tf=sublinear_tf)  # sublinear_tf -> tf = 1 + log(tf)
 
     X = vectorizer.fit_transform(' '.join(sentence) for sentence in sentences)
 
