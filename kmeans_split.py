3ca5542ae73bca1bfe73074eb14ffeac74615952
Flavio Martins <flaviomartins@acm.org>
2017-04-20 20:45:10 +0100

    split wip

--- kmeans_split.py
+++ kmeans_split.py
@@ -13,6 +13,7 @@ import logging
 import plac
 
 from multiprocessing import cpu_count
+from os import path
 from gensim import utils
 from corpus.jsonl import JsonlDirSentences
 from corpus.csv import CsvDirSentences
@@ -36,44 +37,28 @@ def iter_sentences(sentences):
 
 @plac.annotations(
     in_dir=("Location of input directory"),
-    out_loc=("Location of output file"),
+    in_file=("Location of input file"),
+    out_dir=("Location of output directory"),
     n_workers=("Number of workers", "option", "n", int),
     nr_clusters=("Number of clusters", "option", "t", int),
-    nr_iter=("Number of iterations", "option", "i", int),
-    batch_size=("Batch size", "option", "c", int),
     job_size=("Job size in number of lines", "option", "j", int),
     max_docs=("Limit maximum number of documents", "option", "L", int),
     fformat=("By default (ff=jsonl), JSONL format is used."
              "Otherwise (ff='csv'), CSV format is used.", "option", "ff", str),
-    no_lemmas=("Disable Lemmatization.", "flag", "nl", bool),
-    no_minibatch=("Use ordinary k-means algorithm (in batch mode).", "flag", "nm", bool),
-    max_features=("Maximum number of features (dimensions) to extract from text.", "option", "D", int),
-    binary_tf=("Make tf term in tf-idf binary.", "flag", "b", bool),
-    sublinear_tf=("Apply sublinear tf scaling, i.e. replace tf with 1 + log(tf).", "flag", "l", bool),
-    no_idf=("Disable Inverse Document Frequency feature weighting.", "flag", "ni", bool),
-    cosine=("Use cosine distances in place of euclidean distances.", "flag", "cos", bool),
-    jsd=("Use Jensen-Shannon divergence in place of euclidean distances.", "flag", "jsd", bool),
-    kld=("Use Kulkarni's Negative Kullback-Liebler metric in place of euclidean distances.", "flag", "kld", bool),
     verbose=("Print progress reports inside k-means algorithm.", "flag", "v", bool)
 )
-def main(in_dir, out_loc, n_workers=cpu_count()-1, nr_clusters=10, batch_size=1000, nr_iter=100,
-         job_size=1, max_docs=None, fformat='jsonl', no_lemmas=False, max_features=10000, no_minibatch=False,
-         binary_tf=False, sublinear_tf=False, no_idf=False, cosine=False, jsd=False, kld=False, verbose=False):
+def main(in_dir, in_file, out_dir, n_workers=cpu_count()-1, nr_clusters=10, job_size=1, max_docs=None,
+         fformat='jsonl', verbose=False):
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-    lemmatize = not no_lemmas
-    minibatch = not no_minibatch
-    use_idf = not no_idf
     # Set training parameters.
     num_clusters = nr_clusters
-    batchsize = batch_size
-    iterations = nr_iter
 
     ff = fformat.lower()
     if (ff == 'jsonl'):
-        sentences = utils.ClippedCorpus(JsonlDirSentences(in_dir, n_workers, job_size, lemmatize=lemmatize),
+        sentences = utils.ClippedCorpus(JsonlDirSentences(in_dir, n_workers, job_size, lemmatize=False),
                                         max_docs=max_docs)
     elif (ff == 'csv'):
-        sentences = utils.ClippedCorpus(CsvDirSentences(in_dir, n_workers, job_size, lemmatize=lemmatize),
+        sentences = utils.ClippedCorpus(CsvDirSentences(in_dir, n_workers, job_size, lemmatize=False),
                                         max_docs=max_docs)
     else:
         print('Unsupported corpus format specified.')
@@ -83,7 +68,7 @@ def main(in_dir, out_loc, n_workers=cpu_count()-1, nr_clusters=10, batch_size=10
     logger.info("Kmeans Split")
 
     cass = {}
-    with io.open(out_loc, 'rt') as f:
+    with io.open(in_file, 'rt') as f:
         for line in f:
             tid, C = line.split()
             cass[int(tid)] = C
@@ -91,10 +76,10 @@ def main(in_dir, out_loc, n_workers=cpu_count()-1, nr_clusters=10, batch_size=10
     fds = []
     for C in range(num_clusters):
         try:
-            os.makedirs('splits/' + str(C))
-        except:
+            os.makedirs(path.join(out_dir, str(C)))
+        except OSError:
             pass
-        fds.append(io.open('splits/' + str(C) + '/' + str(C) + '.jsonl', 'wt', encoding='utf-8'))
+        fds.append(io.open(path.join(out_dir, str(C), str(C) + '.jsonl'), 'wt', encoding='utf-8'))
 
     for tid, raw, sentence in iter_sentences(sentences):
         if tid in cass:
